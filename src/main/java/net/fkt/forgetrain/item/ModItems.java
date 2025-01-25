3bc27d4d3d96bfd5ae1df23e7a3c9bc3f5242979
fan9704 <cxz123499@gmail.com>
2025-01-26 00:15:38 +0800

    :sparkles: feat: New Item Create \n Alexandrite Gem and Raw Alexandrite Create

---
+++ src/main/java/net/fkt/forgetrain/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.fkt.forgetrain.item;
+
+import net.fkt.forgetrain.ForgeTrain;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    // 註冊延遲物件
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ForgeTrain.MOD_ID);
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            ()-> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
+            () -> new Item(new Item.Properties()));
+    // 建造此 Mod 註冊到 Main Class 的方法
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
