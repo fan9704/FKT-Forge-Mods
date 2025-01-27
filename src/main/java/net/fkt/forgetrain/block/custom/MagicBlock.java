fdf6b89372d7c8eba4be3a7ec06eb2a8bc8223fc
fan9704 <cxz123499@gmail.com>
2025-01-27 16:09:03 +0800

    :sparkles: feat: MagicBlock Apply new CustomizeTag(transformable_items.json)

--- src/main/java/net/fkt/forgetrain/block/custom/MagicBlock.java
+++ src/main/java/net/fkt/forgetrain/block/custom/MagicBlock.java
@@ -1,6 +1,7 @@
 package net.fkt.forgetrain.block.custom;
 
 import net.fkt.forgetrain.item.ModItems;
+import net.fkt.forgetrain.util.ModTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
@@ -34,7 +35,7 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
         if(pEntity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getItem().getItem() == ModItems.RAW_ALEXANDRITE.get()){
+            if(isValidItem(itemEntity.getItem())){
                 itemEntity.setItem(new ItemStack(Items.DIAMOND,itemEntity.getItem().getCount()));
             }
         }
@@ -44,6 +45,10 @@ public class MagicBlock extends Block {
             }
         }
         super.stepOn(pLevel, pPos, pState, pEntity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
