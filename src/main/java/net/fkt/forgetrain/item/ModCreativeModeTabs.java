4bf6911c7cc42adc10ecbf2db0bd58b543059d97
fan9704 <cxz123499@gmail.com>
2025-01-31 11:21:02 +0800

    :sparkles: feat: Armor  Helmet/Chestplate/Leggings/Boots

--- src/main/java/net/fkt/forgetrain/item/ModCreativeModeTabs.java
+++ src/main/java/net/fkt/forgetrain/item/ModCreativeModeTabs.java
@@ -31,6 +31,10 @@ public class ModCreativeModeTabs {
 
                         output.accept(ModItems.ALEXANDRITE_HAMMER.get());
 
+                        output.accept(ModItems.ALEXANDRITE_HELMET.get());
+                        output.accept(ModItems.ALEXANDRITE_CHESTPLATE.get());
+                        output.accept(ModItems.ALEXANDRITE_LEGGINGS.get());
+                        output.accept(ModItems.ALEXANDRITE_BOOTS.get());
                     }).build());
     public static final RegistryObject<CreativeModeTab> ALEXANDRITE_BLOCKS_TAB = CREATIVE_MODE_TABS.register("alexendrite_blocks_tab",
             ()-> CreativeModeTab.builder().icon(()-> new ItemStack(ModBlocks.ALEXANDRITE_BLOCK.get()))
