76a62af85a7c1ac63dcb527442c0df114b02cd0a
fan9704 <cxz123499@gmail.com>
2025-02-03 15:47:20 +0800

    feat: Tree generate in Biome Plains and SAVANNA

--- src/main/java/net/fkt/forgetrain/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/fkt/forgetrain/worldgen/ModPlacedFeatures.java
@@ -1,9 +1,12 @@
 package net.fkt.forgetrain.worldgen;
 
 import net.fkt.forgetrain.ForgeTrain;
+import net.fkt.forgetrain.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -19,6 +22,7 @@ public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> NETHER_ALEXANDRITE_ORE_PLACED_KEY = registerKey("nether_alexandrite_ore_placed");
     public static final ResourceKey<PlacedFeature> END_ALEXANDRITE_ORE_PLACED_KEY = registerKey("end_alexandrite_ore_placed");
 
+    public static final ResourceKey<PlacedFeature> WALNUT_PLACED_KEY = registerKey("walnut_placed");
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
@@ -34,6 +38,10 @@ public class ModPlacedFeatures {
         register(context, END_ALEXANDRITE_ORE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_ALEXANDRITE_ORE_KEY),
                 ModOrePlacement.commonOrePlacement(12,
                         HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(80))));
+        // 每 3 顆有 10% 多 2 個
+        register(context,WALNUT_PLACED_KEY,configuredFeatures.getOrThrow(ModConfiguredFeatures.WALNUT_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3,0.1f,2),
+                        ModBlocks.WALNUT_SAPLING.get()));
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
